de53f0a084b96d0dc73ceecc8d35b3f2566de54a
Michael Lavelle <michael@lavelle.name>
2011-10-13 22:26:54 +0100

    Adding javadoc as part of #1 and adding licence information

--- src/main/java/org/socialsignin/springframework/social/security/signin/SpringSocialSecurityAuthenticationFilter.java
+++ src/main/java/org/socialsignin/springframework/social/security/signin/SpringSocialSecurityAuthenticationFilter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.socialsignin.springframework.social.security.signin;
 
 import java.io.IOException;
@@ -5,6 +20,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 import org.socialsignin.springframework.social.security.userauthorities.UserAuthoritiesService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +33,14 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.stereotype.Component;
 
+/**
+* Processes an SpringSocialSecurity authentication request.
+* Checks for session attribute containing userid and connection as set by SpringSocialSecuritySignInService
+* and returns a UsernamePasswordAuthenticationToken
 
+  This filter by default responds to the URL /authenticate. 
+* @author Michael Lavelle
+*/
 @Component
 @Qualifier("springSocialSecurityAuthenticationFilter")
 public class SpringSocialSecurityAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
